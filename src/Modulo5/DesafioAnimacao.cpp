e69ae14213008f9bb07120107b6fe45a8b519a0a
Eduardo Kuhn <eduardo.kuhn@bancobbcdigital.com.br>
2025-06-16 21:09:00 -0300

    Add pink monster sprite assets and implement character animation logic

--- src/Modulo5/DesafioAnimacao.cpp
+++ src/Modulo5/DesafioAnimacao.cpp
@@ -16,79 +16,284 @@ using namespace std;
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
 
 int setupShader();
-int setupSprite();
-int loadTexture(string filePath);
 
-const GLuint WIDTH = 1920, HEIGHT = 1080;
+const GLuint WIDTH = 600, HEIGHT = 600;
 
 const GLchar *vertexShaderSource = R"(
- #version 400
- layout (location = 0) in vec3 position;
- layout (location = 1) in vec2 texc;
- out vec2 tex_coord;
- 
- uniform mat4 model;
- uniform mat4 projection;
+#version 400
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec2 texc;
 
- void main()
- {
-    tex_coord = vec2(texc.s, 1.0 - texc.t);
+out vec2 tex_coord;
+
+uniform mat4 model;
+uniform mat4 projection;
+uniform vec2 uvOffset;
+uniform vec2 uvScale;
+
+void main()
+{
+    tex_coord = texc * uvScale + uvOffset;
     gl_Position = projection * model * vec4(position, 1.0);
- }
- )";
+}
+)";
 
 const GLchar *fragmentShaderSource = R"(
- #version 400
- in vec2 tex_coord;
- out vec4 color;
- uniform sampler2D tex_buff;
- void main()
- {
-	 color = texture(tex_buff,tex_coord);
- }
- )";
+#version 400
+in vec2 tex_coord;
+out vec4 color;
+
+uniform sampler2D tex_buff;
+
+void main()
+{
+    color = texture(tex_buff, tex_coord);
+}
+)";
 
 class Sprite
 {
-public:
-    Sprite(string path, int shaderID, float relWidth, float relHeight, float xpos = 0.0f, float ypos = 0.0f)
-    {
-        _sprite = loadTexture(path);
-        _vao = setupSprite();
-        _projMat = ortho(0.0f, float(WIDTH), 0.0f, float(HEIGHT), -1.0f, 1.0f);
-        _shaderID = shaderID;
+private:
+    GLuint _vao, _vbo, _ebo, _texID;
+    GLuint _shaderID;
 
-        mat4 model = mat4(1.0f);
-        model = translate(model, vec3(xpos, ypos, 0.0f));
-        model = scale(model, vec3(relWidth, relHeight, 1.0f));
-        _modelMat = model;
+    int _textureWidth, _textureHeight;
+    int _frameWidth, _frameHeight;
+
+    int _framesX, _framesY;
+    int _currentFrame;
+    float _timeSinceLastFrame;
+    float _frameDuration;
+
+public:
+    float _relWidth, _relHeight;
+    Sprite(const char *path, GLuint shaderID, int frameWidth, int frameHeight, int framesX, int framesY, float relWidth, float relHeight)
+        : _shaderID(shaderID), _frameWidth(frameWidth), _frameHeight(frameHeight), _framesX(framesX), _framesY(framesY), _currentFrame(0),
+          _timeSinceLastFrame(0.0f), _frameDuration(0.2f), _relWidth(relWidth), _relHeight(relHeight)
+    {
+        int nrChannels;
+        unsigned char *data = stbi_load(path, &_textureWidth, &_textureHeight, &nrChannels, 4);
+        if (!data)
+        {
+            std::cerr << "Failed to load texture: " << path << std::endl;
+            return;
+        }
+
+        glGenTextures(1, &_texID);
+        glBindTexture(GL_TEXTURE_2D, _texID);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _textureWidth, _textureHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+
+        stbi_image_free(data);
+
+        float vertices[] = {
+            // positions       // tex coords
+            0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+            1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+            1.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 1.0f, 0.0f, 0.0f, 1.0f};
+
+        unsigned int indices[] = {
+            0, 1, 2,
+            2, 3, 0};
+
+        glGenVertexArrays(1, &_vao);
+        glGenBuffers(1, &_vbo);
+        glGenBuffers(1, &_ebo);
+
+        glBindVertexArray(_vao);
+
+        glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+
+        glBindVertexArray(0);
+    }
+
+    void draw(int frameX, int frameY, mat4 modelMat, mat4 projMat)
+    {
+        glUseProgram(_shaderID);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, _texID);
+        glUniform1i(glGetUniformLocation(_shaderID, "tex_buff"), 0);
+
+        float offsetX = float(frameX * _frameWidth) / _textureWidth;
+        float offsetY = float(frameY * _frameHeight) / _textureHeight;
+
+        float scaleX = float(_frameWidth) / _textureWidth;
+        float scaleY = float(_frameHeight) / _textureHeight;
+
+        offsetY = 1.0f - offsetY - scaleY;
+
+        glUniform2f(glGetUniformLocation(_shaderID, "uvOffset"), offsetX, offsetY);
+        glUniform2f(glGetUniformLocation(_shaderID, "uvScale"), scaleX, scaleY);
+
+        glUniformMatrix4fv(glGetUniformLocation(_shaderID, "model"), 1, GL_FALSE, &modelMat[0][0]);
+        glUniformMatrix4fv(glGetUniformLocation(_shaderID, "projection"), 1, GL_FALSE, &projMat[0][0]);
+
+        glBindVertexArray(_vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+    }
+};
+
+enum AnimationState
+{
+    IDLE,
+    WALK,
+    CLIMB
+};
+
+class CharacterController
+{
+private:
+    Sprite _idleSprite;
+    Sprite _walkSprite;
+    Sprite _climbSprite;
+    Sprite *_currentSprite;
+
+    mat4 _modelMat;
+    mat4 _projMat;
+    bool _facingRight;
+
+    float _x, _y;
+    int _frameX, _frameY;
+
+    float _animTimer;
+    float _animSpeed;
+
+public:
+    CharacterController(
+        const char *idlePath,
+        const char *walkPath,
+        const char *climbPath,
+        GLuint shaderID) : _idleSprite(idlePath, shaderID, 32, 32, 4, 1, 32, 32),
+                           _walkSprite(walkPath, shaderID, 32, 32, 4, 1, 32, 32),
+                           _climbSprite(climbPath, shaderID, 32, 32, 4, 2, 32, 32)
+    {
+        _x = WIDTH / 2.0f;
+        _y = HEIGHT / 2.0f;
+        _frameX = 0;
+        _frameY = 0;
+        _currentSprite = &_idleSprite;
+        _facingRight = true;
+        _animTimer = 0.0f;
+        _animSpeed = 0.15f;
+
+        _projMat = glm::ortho(0.0f, float(WIDTH), float(HEIGHT), 0.0f, -1.0f, 1.0f);
+    };
+
+    void handleInput(GLFWwindow *window, float dt)
+    {
+        const float speed = 150.0f;
+        bool isMoving = false;
+
+        if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+        {
+            _x += speed * dt;
+            _currentSprite = &_walkSprite;
+            _facingRight = true;
+            isMoving = true;
+        }
+        else if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+        {
+            _x -= speed * dt;
+            _currentSprite = &_walkSprite;
+            _facingRight = false;
+            isMoving = true;
+        }
+        else if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+        {
+            _y -= speed * dt;
+            _currentSprite = &_climbSprite;
+            isMoving = true;
+        }
+        else
+        {
+            _currentSprite = &_idleSprite;
+        }
+
+        if (isMoving)
+        {
+            animate(dt);
+            if (_currentSprite == &_climbSprite)
+                animateClimb(dt);
+        }
+        else
+        {
+            animateIdle(dt);
+        }
+    }
+
+    void animate(float dt)
+    {
+        _animTimer += dt;
+        if (_animTimer >= _animSpeed)
+        {
+            _animTimer = 0.0f;
+            _frameX++;
+            if (_frameX >= 4)
+                _frameX = 0;
+        }
+    }
+
+    void update(float dt)
+    {
+        _modelMat = mat4(1.0f);
+        _modelMat = translate(_modelMat, vec3(_x, _y, 0.0f));
+        float scaleX = _currentSprite->_relWidth * (_facingRight ? 1.0f : -1.0f);
+        float scaleY = _currentSprite->_relHeight;
+        _modelMat = scale(_modelMat, vec3(scaleX, scaleY, 1.0f));
     }
 
     void draw()
     {
-        glUseProgram(_shaderID);
+        _modelMat = glm::mat4(1.0f);
+        _modelMat = glm::translate(_modelMat, glm::vec3(_x, _y, 0.0f));
 
-        glUniformMatrix4fv(glGetUniformLocation(_shaderID, "model"), 1, GL_FALSE, value_ptr(_modelMat));
-        glUniformMatrix4fv(glGetUniformLocation(_shaderID, "projection"), 1, GL_FALSE, value_ptr(_projMat));
+        float scaleX = _facingRight ? 1.0f : -1.0f;
 
-        glBindVertexArray(_vao);
-        glBindTexture(GL_TEXTURE_2D, _sprite);
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        _modelMat = glm::scale(_modelMat, glm::vec3(scaleX * _currentSprite->_relWidth, _currentSprite->_relHeight, 1.0f));
+
+        _currentSprite->draw(_frameX, _frameY, _modelMat, _projMat);
     }
 
-    void clear()
+    void animateClimb(float dt)
     {
-        glDeleteVertexArrays(1, &_vao);
+        _animTimer += dt;
+        if (_animTimer >= _animSpeed)
+        {
+            _animTimer = 0.0f;
+            _frameX = (_frameX + 1) % 4;
+            _frameY = 2;
+        }
     }
 
-private:
-    GLuint _vao;
-    GLuint _sprite;
-    mat4 _projMat;
-    mat4 _modelMat;
-    int _shaderID;
+    void animateIdle(float dt)
+    {
+        _animTimer += dt;
+        if (_animTimer >= _animSpeed)
+        {
+            _animTimer = 0.0f;
+            _frameX = (_frameX + 1) % 4;
+            _frameY = 0;
+        }
+    }
 };
-
 int main()
 {
     glfwInit();
@@ -145,35 +350,24 @@ int main()
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    vector<Sprite> sprites = {
-        Sprite("../assets/textures/sky.png", shaderID, WIDTH, HEIGHT, WIDTH / 2, HEIGHT / 2),
-        Sprite("../assets/textures/floor.png", shaderID, WIDTH, HEIGHT / 2, WIDTH / 2, HEIGHT / 4),
-        Sprite("../assets/textures/palace.png", shaderID, WIDTH, HEIGHT / 2 + 30, WIDTH / 2, HEIGHT - HEIGHT / 4),
-        Sprite("../assets/textures/gates.png", shaderID, WIDTH, HEIGHT, WIDTH / 2, HEIGHT - HEIGHT / 2),
-        Sprite("../assets/textures/woods.png", shaderID, WIDTH, HEIGHT / 2, WIDTH / 2, HEIGHT / 4),
-        Sprite("../assets/textures/eye.png", shaderID, 400, 400, 500.0, 380.0),
-        Sprite("../assets/textures/skulls.png", shaderID, 100, 100, 1300.0, 200.0),
-        Sprite("../assets/textures/dragon.png", shaderID, 350, 350, 1200.0, 400.0),
-        Sprite("../assets/textures/flower.png", shaderID, 200, 200, 750.0, 150.0),
-    };
+    CharacterController player("../assets/sprites/pinkMonsterIdle.png", "../assets/sprites/pinkMonsterWalk.png", "../assets/sprites/pinkMonsterClimb.png", shaderID);
 
     while (!glfwWindowShouldClose(window))
     {
+
+        double curr_s = glfwGetTime();
+        double elapsed_s = curr_s - prev_s;
+        prev_s = curr_s;
+
+        title_countdown_s -= elapsed_s;
+        if (title_countdown_s <= 0.0 && elapsed_s > 0.0)
         {
-            double curr_s = glfwGetTime();
-            double elapsed_s = curr_s - prev_s;
-            prev_s = curr_s;
+            double fps = 1.0 / elapsed_s;
+            char tmp[256];
+            snprintf(tmp, sizeof(tmp), "Ola Triangulo! -- Rossana\tFPS %.2lf", fps);
+            glfwSetWindowTitle(window, tmp);
 
-            title_countdown_s -= elapsed_s;
-            if (title_countdown_s <= 0.0 && elapsed_s > 0.0)
-            {
-                double fps = 1.0 / elapsed_s;
-                char tmp[256];
-                snprintf(tmp, sizeof(tmp), "Ola Triangulo! -- Rossana\tFPS %.2lf", fps);
-                glfwSetWindowTitle(window, tmp);
-
-                title_countdown_s = 0.1;
-            }
+            title_countdown_s = 0.1;
         }
 
         glfwPollEvents();
@@ -184,19 +378,14 @@ int main()
         glLineWidth(10);
         glPointSize(20);
 
-        for (auto &sprite : sprites)
-        {
-            sprite.draw();
-        }
-
         glUseProgram(shaderID);
 
-        glfwSwapBuffers(window);
-    }
+        player.handleInput(window, elapsed_s);
+        player.update(elapsed_s);
+        player.draw();
 
-    for (auto &sprite : sprites)
-    {
-        sprite.clear();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 
     glfwTerminate();
@@ -248,76 +437,4 @@ int setupShader()
     glDeleteShader(fragmentShader);
 
     return shaderProgram;
-}
-
-int setupSprite()
-{
-    GLfloat vertices[] = {
-        // x   y    z    s     t
-        -0.5, 0.5, 0.0, 0.0, 1.0,  // V0
-        -0.5, -0.5, 0.0, 0.0, 0.0, // V1
-        0.5, 0.5, 0.0, 1.0, 1.0,   // V2
-        0.5, -0.5, 0.0, 1.0, 0.0   // V3
-    };
-
-    GLuint VBO, VAO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *)0);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *)(3 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(1);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    glBindVertexArray(0);
-
-    return VAO;
-}
-
-int loadTexture(string filePath)
-{
-    GLuint texID;
-
-    glGenTextures(1, &texID);
-    glBindTexture(GL_TEXTURE_2D, texID);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-    int width, height, nrChannels;
-
-    unsigned char *data = stbi_load(filePath.c_str(), &width, &height, &nrChannels, 0);
-
-    if (data)
-    {
-        if (nrChannels == 3)
-        {
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        }
-        else
-
-        {
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        }
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-
-    stbi_image_free(data);
-
-    glBindTexture(GL_TEXTURE_2D, 0);
-
-    return texID;
 }
